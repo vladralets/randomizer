849ada54e2ac384a09abcf5af7074dbc1d32d0fc
Vladyslav Ralets <vladralets@gmail.com>
2025-01-15 21:34:52 +0100

    fixed

--- src/components/layout/QuestionsListLayout.tsx
+++ src/components/layout/QuestionsListLayout.tsx
@@ -2,22 +2,13 @@ import { useEffect } from "react";
 import { useQuestionsList } from "../../context/QuestionsListConext";
 import { AccordionList } from "../organisms";
 import { CodeSnippet } from "../atoms";
+import { data } from "../../data";
 
 const QuestionsListLayout = () => {
   const { setQuestions, activeQuestion } = useQuestionsList();
 
   useEffect(() => {
-    fetch("./questions.json")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((data) => {
         setQuestions(data);
-      })
-      .catch((error) => console.error("Error:", error));
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
