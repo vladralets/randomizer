78e4f0f390772fd48524f5512a332587b3f32911
Vladyslav Ralets <vladralets@gmail.com>
2025-01-15 21:43:24 +0100

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
+import { Route, Routes } from "react-router-dom";
+import { Header } from "./components/layout";
 import {
   HomePage,
-  AdminPage,
-  LoginPage,
   NotFoundPage,
-  QuestionsPage,
-  TasksPage,
-  TestsPage,
+  QuestionsPage
 } from "./pages";
-import { Routes, Route } from "react-router-dom";
-import PrivateRoute from "./routes/PrivateRoute";
-import { Header } from "./components/layout";
 
 function App() {
   return (
@@ -17,20 +12,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/tests" element={<TestsPage />} />
         <Route path="/questions" element={<QuestionsPage />} />
-        <Route path="/tasks" element={<TasksPage />} />
-        <Route path="/login" element={<LoginPage />} />
-
-        <Route
-          path="/admin"
-          element={
-            <PrivateRoute>
-              <AdminPage />
-            </PrivateRoute>
-          }
-        />
-
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
